b9720af03a877208289a3930a9068a72fbedab4b
Ziya <ziya@email.com>
2026-05-09 01:02:50 +0300

    Fix Tailwind compilation and React Router Capacitor bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ScriptProvider } from './context/ScriptContext'
 import SplashScreen from './components/SplashScreen'
 import MyScripts from './components/MyScripts'
